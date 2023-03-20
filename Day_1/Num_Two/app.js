f22f4289df50f6c1f9714b2de6d5c85cb18203e9
Ibrahim Rabah <95526530+IbrahimRabah@users.noreply.github.com>
2023-03-20 15:31:40 +0200

    Add files via upload

--- Day_1/Num_Two/app.js
+++ Day_1/Num_Two/app.js
@@ -1,4 +1,11 @@
 var personInfo = require('./Info');
 
-console.log(personInfo.GetAge("Prahim",'2000-01-01')); 
-//console.log(personInfo.GetAge("Prahim",2000)); 
+try 
+  {
+    console.log(personInfo.GetAge("Prahim",'2000-01-01'));
+    //console.log(personInfo.GetAge("Prahim",2000)); 
+  } 
+  catch (error) 
+  {
+    console.error(error.message); 
+  }
